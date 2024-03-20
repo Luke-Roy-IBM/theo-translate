d8a31cc54b331880b8f930cbd922e1fc7c7768bf
Luke Roy <luke.roy@ibm.com>
2024-03-20 16:08:37 +0100

    v1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,30 @@
 <script>
 	import translate from "translate";
+	import {
+		Button,
+		NativeSelect,
+		Textarea,
+		Container,
+		Grid,
+		Stack,
+	} from "@svelteuidev/core";
 	let inputtext = "";
 	let outputtext = "";
 	let inputlang = "de";
 	let outputlang = "en";
-	const languages = {
-		"Deutsch": "de",
-		"Englisch": "en",
-		"Spanisch": "es",
-
-	}
+	/* const languages = {
+		Deutsch: "de",
+		Englisch: "en",
+		Spanisch: "es",
+	}; */
+	const languages = [
+		{ label: "Deutsch", value: "de" },
+		{ label: "Englisch", value: "en" },
+		{ label: "Spanisch", value: "es" },
+	];
+	/*let person = {name: "Theo", age: 15, student: true}
+	let persons = {student: [
+		{name: ""}]}*/
 	translate.engine = "google";
 
 	const text = translate("Hello world", "en");
@@ -25,15 +40,13 @@
 	}
 
 	function translatetext() {
-		const text = translate(inputtext, {from: inputlang, to: outputlang});
+		const text = translate(inputtext, { from: inputlang, to: outputlang });
 		text.then((rsp) => {
 			// console.log(rsp);
 			outputtext = rsp;
 		});
-
 	}
 </script>
-
 
 <h1><center>Translater</center></h1>
 
@@ -42,42 +55,88 @@
 	{count === 1 ? "time" : "times"}
 </button>
 <center>
-	<table style="width: 40%;">
+	<table style="width: 80%;">
 		<tr>
 			<th>
-				<select bind:value={inputlang}>
-				<option value="de">Deutsch</option>
-				<option value="en">Englisch</option>
-				<option value="es">Spanisch</option>
-				</select>
+				<NativeSelect
+					data={languages}
+					bind:value={inputlang}
+					label="Select your output language:"
+				/>
+
+				<!-- <select bind:value={inputlang}>
+					<option value="de">Deutsch</option>
+					<option value="en">Englisch</option>
+					<option value="es">Spanisch</option>
+				</select> -->
 			</th>
 
 			<th>
-				<select bind:value={outputlang}>
-				{#each Object.entries(languages) as [title, lang]}
-					<option value={lang}>{title}</option>
-	
-					
-				{/each}
-				</select>
+				<NativeSelect
+					data={languages}
+					bind:value={outputlang}
+					label="Select your output language:"
+				/>
+
+				<!--  
+					{#each Object.entries(languages) as [title, lang]}
+						<option value={lang}>{title}</option>
+					{/each}
+				</select>-->
 			</th>
 		</tr>
 
 		<tr style="height: 100%;">
-			<td
-				><textarea rows="5" cols="50" bind:value={inputtext}
-				></textarea></td
-			>
-
-			<td
-				><textarea rows="5" cols="50" bind:value={outputtext}
-				></textarea></td
-			>
+			<td>
+				<Textarea placeholder="text" bind:value={inputtext} rows={5} />
+				<!-- 
+					<textarea rows="5" cols="50" bind:value={inputtext}></textarea>
+			 -->
+			</td>
+			<td>
+				<Textarea placeholder="text" bind:value={outputtext} rows={5} />
+				<!-- <textarea rows="5" cols="50" bind:value={outputtext}></textarea> -->
+			</td>
 		</tr>
 	</table>
 
-	<button on:click={translatetext}>translate</button>
+	<!-- <button on:click={translatetext} >translate</button> -->
+	<Button
+		on:click={translatetext}
+		variant="gradient"
+		gradient={{ from: "blue", to: "violet", deg: 90 }}
+		size="sm"
+		uppercase
+	>
+		translate
+	</Button>
 </center>
+
+<Container override={{ bc: "AliceBlue" }}>
+	<Grid>
+		<Grid.Col span={6}>
+			<Stack override={{ height: 300 }}>
+				<NativeSelect
+					data={languages}
+					bind:value={inputlang}
+					label="Select your output language:"
+				/>
+				<Textarea placeholder="text" bind:value={inputtext} rows={5} radius="lg"/>
+			</Stack>
+		</Grid.Col>
+
+		<Grid.Col span={6}>
+			<Stack override={{ height: 300 }}>
+				<NativeSelect
+					data={languages}
+					bind:value={outputlang}
+					label="Select your output language:"
+				/>
+				<Textarea placeholder="text" bind:value={outputtext} rows={5} />
+			</Stack>
+		</Grid.Col>
+	</Grid>
+</Container>
 
 <style>
 	table,
